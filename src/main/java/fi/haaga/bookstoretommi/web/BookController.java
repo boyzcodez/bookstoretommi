f91ce2ff4daf7ae10ecf15cc4343bfa92787974b
Whiteboyz <Korhonen.Tommi@proton.me>
2026-09-15 09:49:34 +0300

    Book edit has been added and booklist.html is now using bootstrap.min.css

--- src/main/java/fi/haaga/bookstoretommi/web/BookController.java
+++ src/main/java/fi/haaga/bookstoretommi/web/BookController.java
@@ -7,17 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fi.haaga.bookstoretommi.domain.Book;
 import fi.haaga.bookstoretommi.domain.BookRepository;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 @Controller
 public class BookController {
@@ -61,5 +55,27 @@ public class BookController {
 
         return "redirect:/index";
     }
-    
+
+    @RequestMapping("/editbook/{id}")
+    public String editBookForm(@PathVariable Long id, Model model) {
+        Book book = bookRepository.findById(id).orElseThrow();
+
+        model.addAttribute("book", book);
+
+        return "editbook";
+    }
+
+    @PostMapping("/editbook/{id}")
+    public String editBook(@PathVariable Long id, Book updatedBook) {
+        Book book = bookRepository.findById(id).orElseThrow();
+
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        book.setPublicationYear(updatedBook.getPublicationYear());
+        book.setIsbn(updatedBook.getIsbn());
+        book.setPrice(updatedBook.getPrice());
+        bookRepository.save(book);
+
+        return "redirect:/index";
+    }
 }
